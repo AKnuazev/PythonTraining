8b8fe9203660235e0d67f49361d5a4a463c8be90
AKnuazev <2536040@gmail.com>
2021-04-05 12:07:24 +0300

    Fixed code formatting

--- task3/PageParser.py
+++ task3/PageParser.py
@@ -1,48 +1,45 @@
 import json
 import requests
 from os import getcwd
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from threading import Thread, Lock
 from time import time, sleep
 from math import ceil
 
-
-# ======================= MULTITHREADING =======================
-
 # Locker
-threadLock = Lock()
+thread_lock = Lock()
 
 
 class AppParsingThread(Thread):
     """ Class for own threads """
-    def __init__(self, apps, keyword, pageData):
+    def __init__(self, apps: list, keyword: str, page_data: list):
         Thread.__init__(self)
+
         self.apps = apps
         self.keyword = keyword
-        self.pageData = pageData
+        self.page_data = page_data
 
     def run(self):
-        appData = []
-        for appBlock in self.apps:
-            data = get_app_data(appBlock, self.keyword)
+        app_data = []
+        for app_block in self.apps:
+            data = get_app_data(app_block, self.keyword)
             if data is not None:
-                appData.append(data)    # Parse page
+                app_data.append(data)    # Parse page
 
-        threadLock.acquire()            # Lock
-        self.pageData.extend(appData)   # Save to global
-        threadLock.release()            # Unlock
+        thread_lock.acquire()            # Lock
+        self.page_data.extend(app_data)  # Save to global
+        thread_lock.release()            # Unlock
 
 
-# ======================= PAGES PARSING =======================
-def get_app_data(appBlock, keyword):
+def get_app_data(app_block: Tag, keyword: str) -> dict:
     """Returns dictionary with app data by its URL
 
         Arguments:
         --------------------
         title   -- app title
-        appURL  -- app URL
+        app_URL  -- app URL
 
         Output:
         --------------------
@@ -52,25 +49,25 @@ def get_app_data(appBlock, keyword):
             "author": str,
             "category": str,
             "description": str
-            "marksNumber": int,
-            "meanMark": float,
+            "marks_number": int,
+            "mean_mark": float,
             "lastUpdate": str,
         }
         or None if smth is wrong
     """
-    baseURL = "https://play.google.com"  # Base part of URL
+    base_URL = "https://play.google.com"  # Base part of URL
     try:
-        title = appBlock.text                           # Get title
-        appURL = appBlock.next_element.attrs['href']    # Get URL
+        title = app_block.text                              # Get title
+        app_URL = app_block.next_element.attrs['href']      # Get URL
         # Check if the keyword is present in title
         if keyword.lower() not in title.lower():
             return None
     except:
         return None  # If for some reason got app with no title -> return None
 
-    appData = {"title": title, "url": baseURL + appURL}     # Dict for app data
+    app_data = {"title": title, "url": base_URL + app_URL}      # Dict for app data
     try:
-        html = requests.get(baseURL + appURL).text          # HTML code of app page
+        html = requests.get(base_URL + app_URL).text            # HTML code of app page
     except:
         return None  # Unavailable url -> return None
 
@@ -80,41 +77,41 @@ def get_app_data(appBlock, keyword):
     # DATA: author, category
     try:
         infoBlock = soup.find('div', class_="qQKdcc")
-        appData["author"] = infoBlock.next.text
-        appData["category"] = infoBlock.text[len(appData["author"]):]
+        app_data["author"] = infoBlock.next.text
+        app_data["category"] = infoBlock.text[len(app_data["author"]):]
 
     except:
-        appData["author"] = None
-        appData["category"] = None
+        app_data["author"] = None
+        app_data["category"] = None
 
     # DATA: description
     try:
         infoBlock = soup.find('div', class_="DWPxHb")
-        appData["description"] = infoBlock.next.next.text
+        app_data["description"] = infoBlock.next.next.text
     except:
-        appData["description"] = None
+        app_data["description"] = None
 
-    # DATA: meanMark, marksNumber
+    # DATA: mean_mark, marks_number
     try:
         infoBlock = soup.find('div', class_="K9wGie")
-        appData["meanMark"] = float(infoBlock.next.text)
+        app_data["mean_mark"] = float(infoBlock.next.text)
         marksNumberText = infoBlock.next.next_sibling.next_sibling.text
-        appData["marksNumber"] = int(marksNumberText.replace(",", "")[:-5])
+        app_data["marks_number"] = int(marksNumberText.replace(",", "")[:-5])
     except:
-        appData["meanMark"] = None
-        appData["marksNumber"] = None
+        app_data["mean_mark"] = None
+        app_data["marks_number"] = None
 
     # DATA: lastUpdate
     try:
         infoBlock = soup.find('div', class_="IxB2fe")
-        appData["lastUpdate"] = infoBlock.next.text.replace("Updated", "")
+        app_data["lastUpdate"] = infoBlock.next.text.replace("Updated", "")
     except:
-        appData["lastUpdate"] = None
+        app_data["lastUpdate"] = None
 
-    return appData
+    return app_data
 
 
-def get_page_data(html, keyword):
+def get_page_data(html: str, keyword: str) -> list:
     """ Returns a list of dictionaries with data about applications on the page
 
         Arguments:
@@ -126,17 +123,17 @@ def get_page_data(html, keyword):
         --------------------
         [ app1Data, app2Data, ... ]
     """
-    pageData = []  # List for apps data
+    page_data = []  # List for apps data
 
-    startTime = time()                                  # Getting apps list starts
-    soup = BeautifulSoup(html, features='lxml')         # Object for parsing the page code
-    apps = soup.find_all('div', class_='Q9MA7b')        # Getting a list of found application blocks
-    print("• Getting apps list:\t", time() - startTime) # Getting apps list ends
+    start_time = time()                                     # Getting apps list starts
+    soup = BeautifulSoup(html, features='lxml')             # Object for parsing the page code
+    apps = soup.find_all('div', class_='Q9MA7b')            # Getting a list of found application blocks
+    print("• Getting apps list:\t", time() - start_time)    # Getting apps list ends
 
-    startTime = time()  # App parsing starts
+    start_time = time()  # App parsing starts
 
     # Calculations for threads creation
-    THREADS_NUMBER = 8  # Based on a small research - gives best efficiency (about 30 apps on thread on popular words)
+    THREADS_NUMBER = 8  # Based on a small research - gives best efficiency (about 30 apps per thread on popular words)
     step = ceil(len(apps)/THREADS_NUMBER)
 
     # Threads creation
@@ -144,23 +141,24 @@ def get_page_data(html, keyword):
     for i in range(THREADS_NUMBER):
         start = step*i
         end = step*(i+1) if step*(i+1) < len(apps) else len(apps)
-        newThread = AppParsingThread(apps[start:end], keyword, pageData)     # Create
-        newThread.start()                                                    # Start
-        pool.append(newThread)                                               # Save to list
+        new_thread = AppParsingThread(apps[start:end], keyword, page_data)   # Create
+        new_thread.start()                                                   # Start
+        pool.append(new_thread)                                              # Save to list
 
     # Wait for threads to end
     for thread in pool:
         thread.join()
 
-    print("• Parsing apps pages:\t", time() - startTime)  # App parsing ends
+    print("• Parsing apps pages:\t", time() - start_time)  # App parsing ends
 
-    print("\n============ SEARCH STATISTIC ============")
+    print("\nSEARCH STATISTIC:")
     print("• Total apps found:\t", len(apps))
-    print("• Relevant apps:\t", len(pageData))
-    return pageData
+    print("• Relevant apps:\t", len(page_data))
+
+    return page_data
 
 
-def get_page_html(URL):
+def get_page_html(URL: str) -> str:
     """ Returns HTML code of page with apps, that were found by keyword (using Chrome driver)
 
         Arguments:
@@ -171,33 +169,32 @@ def get_page_html(URL):
         --------------------
         <html>...</html>
     """
-    print("\n============= TIME STATISTIC =============")
+    print("\nTIME STATISTIC:")
     SCROLL_PAUSE_TIME = 1  # Time to wait for page to load new apps
 
     # Set up browser
-    startTime = time()
+    start_time = time()
     options = Options()
     options.headless = True  # Disable opening window
     browser = webdriver.Chrome(executable_path=getcwd() + "/chromedriver", options=options)
 
     browser.get(URL)  # Load page by URL
 
-    lastHeight = None
-    newHeight = browser.execute_script("return document.body.scrollHeight")  # Get scroll height
-    while not lastHeight == newHeight:
-        lastHeight = newHeight
+    last_height = None
+    new_height = browser.execute_script("return document.body.scrollHeight")     # Get scroll height
+    while not last_height == new_height:
+        last_height = new_height
         script = "window.scrollTo(0, document.body.scrollHeight);"  # Script to scroll down
-        browser.execute_script(script)  # Execute script
+        browser.execute_script(script)                              # Execute script
 
-        sleep(SCROLL_PAUSE_TIME)  # Wait for page to load new apps
+        sleep(SCROLL_PAUSE_TIME)                                    # Wait for page to load new apps
 
-        newHeight = browser.execute_script("return document.body.scrollHeight")  # Get new height
+        new_height = browser.execute_script("return document.body.scrollHeight")  # Get new height
 
-    print("• Getting page code:\t", time()-startTime)
+    print("• Getting page code:\t", time()-start_time)
     return browser.page_source
 
 
-# ======================= CONTROL =======================
 def main():
     print("Welcome to Google Play Store parsing app")
     print("-------------------------------------------\n")
@@ -207,10 +204,10 @@ def main():
     URL = "https://play.google.com/store/search?q=" + keyword + "&c=apps"
     html = get_page_html(URL)
 
-    pageData = get_page_data(html, keyword)
+    page_data = get_page_data(html, keyword)
 
-    print("\n=============== APPS DATA: ===============")
-    print(json.dumps(pageData, indent=4, ensure_ascii=False))
+    print("\nAPPS DATA:")
+    print(json.dumps(page_data, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
